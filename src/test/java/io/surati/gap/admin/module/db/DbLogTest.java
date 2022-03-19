4e1dc9e015e068565c8cb0ccd62f1ffdb348841a
Brou Marcel <broumarcel87@gmail.com>
2022-03-19 12:50:06 +0000

    #7 : Test DbLog

--- src/test/java/io/surati/gap/admin/module/db/DbLogTest.java
+++ src/test/java/io/surati/gap/admin/module/db/DbLogTest.java
@@ -2,6 +2,9 @@ package io.surati.gap.admin.module.db;
 
 import com.lightweight.db.EmbeddedPostgreSQLDataSource;
 import com.lightweight.db.LiquibaseDataSource;
+
+import io.surati.gap.admin.module.api.EventLog;
+
 import javax.sql.DataSource;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
@@ -52,5 +55,60 @@ final class DbLogTest {
                     evt.level() == Level.INFO
             )
         );
+    }
+    
+    @Test
+    void addErrorEvent() {
+        final String message = "HTTP Error 500";
+        final String details = "HTTP Error 500 (Internal Server Error).";
+        new DbLog(
+            this.source,
+            DbLogTest.AUTHOR,
+            DbLogTest.IP_ADDRESS
+        ).error(message, details);
+        MatcherAssert.assertThat(
+            new DbEventLogs(this.source).get(1L),
+            new Satisfies<>(
+                evt -> evt.author().equals(DbLogTest.AUTHOR) &&
+                    evt.ipAddress().equals(DbLogTest.IP_ADDRESS) &&
+                    evt.message().equals(message) &&
+                    evt.level() == Level.SEVERE
+            )
+        );
+    }
+    
+    @Test
+    void addWarningEvent() {
+        final String message = "Ouh! The username is invalid.";
+        new DbLog(
+            this.source,
+            DbLogTest.AUTHOR,
+            DbLogTest.IP_ADDRESS
+        ).warning(message);
+        MatcherAssert.assertThat(
+            new DbEventLogs(this.source).get(1L),
+            new Satisfies<>(
+                evt -> evt.author().equals(DbLogTest.AUTHOR) &&
+                    evt.ipAddress().equals(DbLogTest.IP_ADDRESS) &&
+                    evt.message().equals(message) &&
+                    evt.level() == Level.WARNING
+            )
+        );
+    }
+    
+    @Test
+    public void iterate() {
+    	final Iterable<EventLog> eventlog = new DbLog(this.source, DbLogTest.AUTHOR, DbLogTest.IP_ADDRESS).iterate();
+    	for (EventLog currenteventlog : eventlog) {
+            MatcherAssert.assertThat(
+                    new DbEventLogs(this.source).get(1L),
+                    new Satisfies<>(
+                        evt -> evt.author().equals(DbLogTest.AUTHOR) &&
+                            evt.ipAddress().equals(DbLogTest.IP_ADDRESS) &&
+                            evt.message().equals(currenteventlog.message()) &&
+                            evt.level() == currenteventlog.level()
+                    )
+                );
+		}
     }
 }
