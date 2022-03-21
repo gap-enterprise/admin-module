42f8186cc8bab95648386319a9de0e0c6d522753
Brou Marcel <broumarcel87@gmail.com>
2022-03-21 00:33:13 +0000

    #19 : Test DbUsers --v2

--- src/test/java/io/surati/gap/admin/module/db/DbUsersTest.java
+++ src/test/java/io/surati/gap/admin/module/db/DbUsersTest.java
@@ -16,18 +16,15 @@
  */
 package io.surati.gap.admin.module.db;
 
-import java.util.regex.Matcher;
-
 import javax.sql.DataSource;
-
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.llorllale.cactoos.matchers.Satisfies;
-
 import com.lightweight.db.EmbeddedPostgreSQLDataSource;
 import com.lightweight.db.LiquibaseDataSource;
-
 import io.surati.gap.admin.module.api.User;
 import io.surati.gap.admin.module.api.Users;
 
@@ -62,4 +59,113 @@ final class DbUsersTest {
 			)
 		);
 	}
+	
+	@Test
+	public void hasUserByItsLogin() {
+		final String name = "Administrateur";
+		final String login = "admin-gap";
+		final String password = "gap2022";
+		users.register(name, login, password);
+		MatcherAssert.assertThat(
+			users.has("admin-gap"),
+			Matchers.is(true)
+		);
+		MatcherAssert.assertThat(
+			users.has("brou87"),
+			Matchers.is(false)
+		);
+	}
+	
+	@Test
+	public void hasUserByItsId() {
+		final String name = "Administrateur";
+		final String login = "admin-gap";
+		final String password = "gap2022";
+		users.register(name, login, password);
+		MatcherAssert.assertThat(
+			users.get(login).id(),
+			Matchers.equalTo(1L)
+		);
+	}
+	
+	@Test
+	void countTotalNumberOfUsers() {
+		final String[] names = { "Mentor", "Guest" };
+		final String[] logins = { "mentor", "guest" };
+		final String password = "gap2022";
+		users.register(names[0], logins[0], password);
+		users.register(names[1], logins[1], password);
+		MatcherAssert.assertThat(
+            "Application should have two registrations.",
+            users.count(),
+            new IsEqual<>(Long.valueOf(logins.length))
+        );
+	}
+	
+	@Test
+	void authenticateUserWithNonEncryptedPassword() {
+		final String name = "Administrateur";
+		final String login = "admin-gap";
+		final String password = "gap2022";
+		users.register(name, login, password);
+		MatcherAssert.assertThat(
+			users.authenticate(login, password),
+			Matchers.is(true)
+		);
+	}
+	
+	@Test
+	void authenticateUserWithEncryptedPassword() {
+		final String name = "Administrateur";
+		final String login = "admin-gap";
+		final String password = "gap2022";
+		users.register(name, login, password);
+		MatcherAssert.assertThat(
+			users.authenticatePwdEncrypted(login, password),
+			Matchers.is(false)
+		);
+	}
+	
+	/*@Test
+	void removeUser() {
+		final String name = "Administrateur";
+		final String login = "admin-gap";
+		final String password = "gap2022";
+		users.register(name, login, password);
+		final User user = users.get(login);
+		users.remove(user.id());
+		MatcherAssert.assertThat(
+			user.id(),
+			Matchers.equalTo(0L)
+		);
+	}*/
+	
+    @Test
+    void iterate() {
+    	final String[] names = { "Mentor", "Guest" };
+		final String[] logins = { "mentor", "guest" };
+		final String password = "gap2022";
+		users.register(names[0], logins[0], password);
+		users.register(names[1], logins[1], password);
+		MatcherAssert.assertThat(
+            "Application should have two registrations.",
+            users.count(),
+            new IsEqual<>(Long.valueOf(logins.length))
+        );
+		int idx = logins.length;
+		for (final User user : users.iterate()) {
+			idx -= 1;
+			final String login = logins[idx];
+            final String name = names[idx];
+			MatcherAssert.assertThat(
+				"User should match in descending order.",
+				user,
+				new Satisfies<>(
+						usr -> usr.name().equals(user.name()) &&
+							   usr.login().equals(user.login()) &&
+							   usr.password().equals(user.password())
+				)
+			);
+		}
+    }
 }
