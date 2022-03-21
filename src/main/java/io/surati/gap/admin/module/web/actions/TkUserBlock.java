8e8bfe087ae421509cc7aab4a8fa5e61fad1b790
Olivier B. OURA <baudoliver7@gmail.com>
2022-03-21 13:13:26 +0000

    feat: add takes related to module `Admin`
    Fix: #18

---
+++ src/main/java/io/surati/gap/admin/module/web/actions/TkUserBlock.java
@@ -0,0 +1,88 @@
+/**
+MIT License
+
+Copyright (c) 2021 Surati
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */  
+package io.surati.gap.admin.module.web.actions;
+
+import io.surati.gap.admin.module.api.User;
+import io.surati.gap.admin.module.db.DbUsers;
+import io.surati.gap.admin.module.rq.RqUser;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.facets.flash.RsFlash;
+import org.takes.facets.forward.RsForward;
+import org.takes.rq.RqHref;
+
+import javax.sql.DataSource;
+import java.util.logging.Level;
+
+/**
+ * Take that block or unblock an user.
+ *
+ * <p>The class is immutable and thread-safe.</p>
+ */
+public final class TkUserBlock implements Take {
+	
+	/**
+	 * Database
+	 */
+	private final DataSource source;
+	
+	/**
+	 * Ctor.
+	 * @param source DataSource
+	 */
+	public TkUserBlock(final DataSource source) {
+		this.source = source;
+	}
+	
+	@Override
+	public Response act(Request req) throws Exception {		
+		final User currentUser = new RqUser(this.source, req);
+		final Long id = Long.parseLong(new RqHref.Smart(req).single("id"));
+		
+		if(currentUser.id().equals(id)) 
+			throw new IllegalArgumentException("Vous ne pouvez pas effectuer cette action !");
+		
+		final boolean enable = Boolean.parseBoolean(new RqHref.Smart(req).single("enable"));
+		
+		final User user = new DbUsers(source).get(id);
+		user.block(enable);
+		
+		final String msg;
+		if(enable) {
+			msg = String.format("L'utilisateur a été bloqué avec succès !");
+		} else {
+			msg = String.format("L'utilisateur a été debloqué avec succès !");
+		}
+		
+		return new RsForward(
+			new RsFlash(
+					msg,
+				Level.INFO
+			),
+			"/user"
+		);	
+	}
+	
+}
---
+++ src/main/java/io/surati/gap/admin/module/web/xe/XeUsersJson.java
@@ -0,0 +1,66 @@
+/**
+MIT License
+
+Copyright (c) 2021 Surati
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */ 
+package io.surati.gap.admin.module.web.xe;
+
+import io.surati.gap.admin.module.api.User;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonStructure;
+import java.io.IOException;
+import org.takes.rs.RsJson;
+
+public final class XeUsersJson implements RsJson.Source {
+
+	private final Long count;
+	private final Iterable<User> items;
+	
+	public XeUsersJson(final Iterable<User> items, final Long count) {
+		this.count = count;
+		this.items = items;
+	}
+	
+	@Override
+	public JsonStructure toJson() throws IOException {
+		return Json.createObjectBuilder()
+		   .add("items", toJson(this.items))
+           .add("count", this.count)
+		   .build();
+	}
+	
+	private static JsonArray toJson(final Iterable<User> items) throws IOException {
+		JsonArrayBuilder builder = Json.createArrayBuilder();
+		for (User item : items) {
+			builder.add(Json.createObjectBuilder()
+				.add("id", item.id())
+				.add("name", item.name())
+				.add("login", item.login())
+				.add("profile", item.profile().name())
+                .add("profile_id", item.profile().id())
+           );
+		}
+		return builder.build();
+	}
+
+}
